6be0501004a8ff3080d40175bfc5055a3cb74e3c
default <bianca.bertinato@stu.ocsb.ca>
2025-11-30 21:38:53 +0000

    added number pattern

--- main.go
+++ main.go
@@ -5,29 +5,21 @@
  * @fileoverview This program uses nested loop to print a pattern of numbers.
  */
 
-// Package main prints a number pattern using nested loops.
 package main
 
 import "fmt"
 
-// main runs the program.
 func main() {
 	var numRows int
 
-	// number of rows
 	fmt.Print("How many rows would you like? ")
 	fmt.Scan(&numRows)
 
-	// outer loop
 	for i := 1; i <= numRows; i++ {
 		rowString := ""
-
-		// inner loop
 		for j := 1; j <= i; j++ {
 			rowString += fmt.Sprintf("%d ", j)
 		}
-
-		// output
 		fmt.Println(rowString)
 	}
 
