40196a1772ab8ddaaca40eada2f5a038f504ee1f
default <bianca.bertinato@stu.ocsb.ca>
2025-11-30 21:25:35 +0000

    added numbered rows in pattern

--- main.go
+++ main.go
@@ -19,16 +19,16 @@ func main() {
 
 	// outer loop
 	for i := 1; i <= numRows; i++ {
-		  rowString := ""
+		rowString := ""
 
-		  // inner loop
-		  for j := 1; j <= i; j++ {
-			    rowString += fmt.Sprintf("%d ", j)
-		  }
+		// inner loop
+		for j := 1; j <= i; j++ {
+			rowString += fmt.Sprintf("%d ", j)
+		}
 
-		  // output
-		  fmt.Println(rowString)
-}
+		// output
+		fmt.Println(rowString)
+	}
 
-fmt.Println("Done.")
+	fmt.Println("Done.")
 }
